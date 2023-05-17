b5d297a0fd8a0b00106a1d80632835469b2e0849
vivid-cpreston <cpreston@vividsolutions.com>
2023-05-17 16:45:33 -0700

    Added additional CHEFS files

--- openshift/openshift-bc.sh
+++ openshift/openshift-bc.sh
@@ -1,2 +1,6 @@
+export NAMESPACE=ec2c6e-$TARGET_ENV
+
+oc process -n $NAMESPACE -f app.bc.yaml -p REPO_NAME=nr-brmb-chefs -p JOB_NAME=master -p SOURCE_REPO_URL=https://github.com/vivid-cpreston/nr-brmb-chefs.git -p SOURCE_REPO_REF=master -p ROUTE_PATH=vue -o yaml | oc apply -n $NAMESPACE -f -
+
 oc start-build -n ec2c6e-$TARGET_ENV nr-brmb-chefs --follow
 oc tag -n ec2c6e-$TARGET_ENV nr-brmb-chefs:latest nr-brmb-chefs:master
